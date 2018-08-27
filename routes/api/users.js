73b0b149f92b88249cf2bd097843ae2903908d23
kakaz <kakazoo209@gmail.com>
2018-08-27 23:02:10 +0200

    implement login form

--- routes/api/users.js
+++ routes/api/users.js
@@ -50,4 +50,30 @@ router.post('/register', (req, res) => {
     });
 })
 
+//@route GET api/usrss/login
+//@desc Login User / returning JWT token
+//@access Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //Find user by Email
+  User.findOne({email})
+    .then(user => {
+      if(!user){
+        return res.status(404).json({email: 'User not found'});
+      }
+
+      //Check password(compareなんていう便利なメソッドあるんけ。。。)
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if(isMatch){
+            res.json({msg: "Success"});
+          }else{
+            return res.status(400).json({password: 'password incorrect'})
+          }
+        })
+    });
+})
+
 module.exports = router;
