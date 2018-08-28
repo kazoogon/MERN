7565af06d4876f472e930a5af337420c7d21988c
kakaz <kakazoo209@gmail.com>
2018-08-28 06:35:39 +0200

    implement more Profile API routs (all, user/:user_id, handle/:handle)

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -34,6 +34,59 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
 });
 module.exports = router;
 
+//@route GET api/profile/handle/;handle
+//@desc Get profile by handle
+//@access Public
+router.get('/handle/:handle', (req, res) => {
+  const errors = {};console.log("req" + req);
+  Profile.findOne({ handle: req.params.handle })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if(!profile){
+        errors.noprofile = 'There is no profile for this user';
+        res.status.json(errors);
+      }
+console.log("profile" + profile);
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+//@route GET api/profile/user/:user_id
+//@desc Get profile by handle
+//@access Public
+router.get('/user/:user_id', (req, res) => {
+  const errors = {};
+  Profile.findOne({ handle: req.params.user_id })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if(!profile){
+        errors.noprofile = 'There is no profile for this user';
+        res.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json({ profile: "there is no profile for this user" }));
+});
+
+//@route GET api/profile/all
+//@desc Get all profiles
+//@access Public
+router.get('/all', (req, res) => {
+  const errors = {};
+  Profile.find()
+    .populate('user', ['name', 'avatar'])
+    .then(profiles => {
+      if(!profiles){
+        errors.noprofile = 'THere are no profiles';
+        return res.status(404).json(errors);
+      }
+      res.json(profiles);
+    })
+    .catch(err => res.status(404).json({ profile: "there are no profiles" }));
+})
+
 //@route POST api/profile
 //@desc Create User profile
 //@access Private
