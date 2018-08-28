6cebb026e25ae711ee74387167df83504140d1c4
kakaz <kakazoo209@gmail.com>
2018-08-28 16:24:06 +0200

    implement post get and delete route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -12,6 +12,48 @@ const validatePostInput = require('../../validation/post');
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: 'Posts Works' }));
 
+// @route   GET api/posts
+// @desc    Get posts
+// @access  Public
+router.get('/', (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err = res.status(404).json({ nopostsfound: 'No posts found' }));
+});
+
+// @route   GET api/posts/:id
+// @desc    Get posts by id
+// @access  Public
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err = res.status(404).json({nopostfound: 'No post found with that ID'}));
+});
+
+// @route   DLETE api/posts/:id
+// @desc    Get post
+// @access  Public
+router.delete(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        Post.findById(req.params.id)
+          .then(post => {
+            //check for post owner
+            if(post.user.toString() != req.user.id){
+              return res.status(401).json({ notauthorized: 'User not authorized' })
+            }
+
+            //DELETE
+            post.remove().then(() => res.json({ success: true }));
+          })
+          .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+      })
+});
+
 // @route   POST api/posts
 // @desc    Create post
 // @access  Private
