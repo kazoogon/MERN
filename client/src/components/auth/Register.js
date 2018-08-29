1027f4cadb04406a2579761ceb906347301ae4c6
kakaz <kakazoo209@gmail.com>
2018-08-29 03:18:22 +0200

    implement action,reducer part of register form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import axios from 'axios';
 import classnames from 'classnames'; //angularではdefaultであるらいい
+import { connect } from 'react-redux'; //reactのcomponentがreduxのstoreの値取得できるようにしてくれる
+import { registerUser } from '../../actions/authActions';
 
 class Register extends Component {
   constructor() {
@@ -32,16 +35,20 @@ class Register extends Component {
       password2: this.state.password2,
     }
 
-    axios.post('/api/users/register', newUser)
-      .then(res => console.log(res.data))
-      .catch(err => this.setState({ errors: err.response.data }))
+    this.props.registerUser(newUser);
+
+    // axios.post('/api/users/register', newUser)
+    //   .then(res => console.log(res.data))
+    //   .catch(err => this.setState({ errors: err.response.data }))
   }
 
   render(){
     const{ errors } = this.state;
+    const { user } = this.props.auth;
 
     return(
       <div className="register">
+        { user ? user.name : null }
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
@@ -115,4 +122,13 @@ class Register extends Component {
   }
 }
 
-export default Register;
+Register.PropTypes = {
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+  auth: state.auth
+})
+
+export default connect(mapStateToProps,{ registerUser })(Register);
